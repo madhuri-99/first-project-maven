8836cb90c4e4309dadfa18076d57a939597890a0
V Madhuri <madhuri-99>
2022-03-22 12:09:34 +0530

    added first commit

---
+++ src/main/java/devops/practice/first_project_maven/App.java
@@ -0,0 +1,13 @@
+package devops.practice.first_project_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
